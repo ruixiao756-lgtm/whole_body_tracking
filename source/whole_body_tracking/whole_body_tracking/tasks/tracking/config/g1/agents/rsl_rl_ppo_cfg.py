547b9946afe2f86de32d04f900144614160bc033
noname7321 <112142333+noname7321@users.noreply.github.com>
2026-03-10 00:44:10 +0800

    还是同一个加大版

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/agents/rsl_rl_ppo_cfg.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/config/g1/agents/rsl_rl_ppo_cfg.py
@@ -16,7 +16,7 @@ class G1FlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
-        value_loss_coef=1.0,
+        value_loss_coef=1.08,
         use_clipped_value_loss=True,
         clip_param=0.2,
         entropy_coef=0.0054,
